728ffcd1dd90cc460f8433b3f8d2ea9c5844eefe
Gron516 <Gron516@gmail.com>
2024-05-11 13:57:35 +0300

    Сделали подключение к Api, делаем вывод всех результатов

---
+++ Models/DrugRecipe.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeService.Models
+{
+    public class DrugRecipe
+    {
+        public string RusName { get; set; }
+        public string ZipInfo { get; set; }
+        public Document Document { get; set; }
+    }
+}
--- Services/CheckService.cs
+++ Services/CheckService.cs
@@ -1,20 +1,24 @@
 ﻿using RecipeService.Models;
+using System.Net.Http;
+using System.Text.Json;
+using System.Xml.Linq;
 
 namespace RecipeService.Services
 {
-    public static class CheckService
+public static class CheckService
     {
-        public static string GetRecipe(string name)
+        public static async Task<string> GetRecipe(string name)
         {
 
             // получение данных
             using (ApplicationContext db = new ApplicationContext())
             {
                 // получаем объекты из бд и выводим на консоль
-                var drug = db.Drugs.Where(drug => drug.Name.ToLower() == name).SingleOrDefault();
+                var drug = db.Drugs.Where(drug => drug.Name.ToLower() == name.ToLower()).SingleOrDefault();
                 if (drug == null)
                 {
-                    return "такого препарата нет в базе данных";
+                    var recipe = await GetRecipeWithApi(name.ToLower());
+                    return "";//recipe;
                 }
                 if (drug.Recipe == null)
                 {
@@ -26,5 +30,39 @@ namespace RecipeService.Services
 
 
         }
+        
+        public static async Task<List<DrugRecipe>> GetRecipeWithApi(string name) 
+        {
+            var drugRecipes = new List<DrugRecipe>();
+            HttpClient httpClient = new HttpClient();
+            // устанавливаем заголовк
+            httpClient.DefaultRequestHeaders.Add("X-Token", "iQpsJ5IHWdPv");
+            int currentPage = 0;
+            int pageCount = 1;
+            do
+            {
+                currentPage++;
+                var serverAddress = $"https://www.vidal.ru/api/rest/v1/product/list?filter[name]={name}&page={currentPage}";
+                Console.WriteLine(serverAddress);
+                // выполняем запрос
+                using (var responseHttp = await httpClient.GetAsync(serverAddress))
+                {
+                    var content = await responseHttp.Content.ReadFromJsonAsync<Response>();
+                    //распарсит json
+                    //var response = JsonSerializer.Deserialize<Response> (content); не актуально?
+                    var drugRecipeArray = content.Products;
+                    pageCount = content.Pagination.PageCount;
+                    foreach (var recipe in drugRecipeArray)
+                        Console.WriteLine($"RusName:{recipe.RusName}; ZipInfo:{recipe.ZipInfo}; Dosage:{recipe.Document.Dosage}; ");
+                }
+            }
+            while (pageCount > currentPage);
+            //var drugaddRecipeArray = contentDocument.Document;
+           // foreach (var recipeadd in drugaddRecipeArray)
+                //Console.WriteLine($"Dosage:{recipeadd.Dosage}; ");
+            //drugRecipes.Add(DrugRecipe); не актуально?
+            return drugRecipes.ToList();
+        } 
     }
+
 }
